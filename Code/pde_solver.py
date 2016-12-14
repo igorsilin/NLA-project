84a8887e50fa8ba3cd80c93115749a678735bedd
igorsilin <siliniv@gmail.com>
2016-12-15 01:40:32 +0300

    experiment updated

--- Code/pde_solver.py
+++ Code/pde_solver.py
@@ -6,17 +6,17 @@ import numpy as np
 
 class PDE_Solver():
     def __init__(self, sigma=1, V=None, f=None, T=0, n=10, a_x=10, M=10):
-        self.sigma = sigma		# float, must be positive; diffusion coefficient
-        self.V = V		# function of 2 variables: x, t; dissipation
-        self.f = f		# function of 1 variable: x; initial condition
-        self.T = T		# float; final moment of time
-        self.n = n		# int; number of time steps
-        self.a_x = a_x		# float; right bound of coordinate segment
-        self.M = M	# int; number of coordinate segments on the final iteration
-        self.u = None		# numerical solution
+        self.sigma = sigma      # float, must be positive; diffusion coefficient
+        self.V = V      # function of 2 variables: x, t; dissipation
+        self.f = f      # function of 1 variable: x; initial condition
+        self.T = T      # float; final moment of time
+        self.n = n      # int; number of time steps
+        self.a_x = a_x      # float; right bound of coordinate segment
+        self.M = M  # int; number of coordinate segments on the final iteration
+        self.u = None       # numerical solution
         self.x = np.linspace(-1 * a_x, a_x, M + 1)
     
-	# Finds a numerical solution of PDE
+    # Finds a numerical solution of PDE
     def Solve(self):
         T = self.T
         n = self.n
@@ -27,13 +27,15 @@ class PDE_Solver():
         sigma = self.sigma
         
         tau = np.linspace(0, T, n + 1)
-        w = self.Trapezoid_Weights(n, dt)
-        
+        #w = self.Trapezoid_Weights(n, dt)
+        w = self.Simpson_Weights(n, dt)
+    
         x = np.linspace(-1 * (n + 1) * a_x, (n + 1) * a_x, (n + 1) * M + 1)
         F = self.f(x)
         
-        mu = self.Trapezoid_Weights(M, h_x)
-        xi = np.linspace(-a_x, a_x, M + 1)
+        #mu = self.Trapezoid_Weights(M, h_x)
+        mu = self.Simpson_Weights(M, h_x)
+    xi = np.linspace(-a_x, a_x, M + 1)
         alpha = 1. / 4 / sigma / dt
         p = np.sqrt(alpha / np.pi) * np.exp(-1. * alpha * xi**2)
         mu = mu * p
@@ -50,7 +52,7 @@ class PDE_Solver():
     
         return
     
-	# Weights corresponding to trapezoid rule quadrature
+    # Weights corresponding to trapezoid rule quadrature
     def Trapezoid_Weights(self, number_of_segments, step):
         weights = step * np.ones(number_of_segments + 1)
         weights[0] *= 0.5
@@ -58,8 +60,8 @@ class PDE_Solver():
         
         return weights
     
-	# Convolution via FFT sum_j Phi[i, j] mu_j
-	# Phi[i, j] = Phi[i + j]
+    # Convolution via FFT sum_j Phi[i, j] mu_j
+    # Phi[i, j] = Phi[i + j]
     def Convolve(self, Phi, mu):
         c1 = Phi[:self.M]
         c2 = Phi[self.M:]
@@ -69,3 +71,14 @@ class PDE_Solver():
         b = np.concatenate([mu[::-1], np.zeros(len_c - len_mu)], axis=0)
         F = np.fft.ifft(np.fft.fft(c) * np.fft.fft(b)).real
         return F[:len_c - self.M]
+
+    # Weights corresponding to Simpson rule quadrature
+    # Number of segments must be even!!!
+    def Simpson_Weights(self, number_of_segments, step):
+        repeats = number_of_segments // 2
+        weights = np.tile(np.array([2, 4]), repeats)
+        weights = np.concatenate([weights, np.ones(1)], axis=0)
+        weights[0] = 1
+        weights *= (step / 3)
+        
+        return weights
